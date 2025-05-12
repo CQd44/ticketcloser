0bf328a4d06d2b2cd12c16349431f1b0137ed2aa
CQd44 <will.clay.young@gmail.com>
2025-05-12 16:25:09 -0500

    Add files via upload

--- ticket_closer.py
+++ ticket_closer.py
@@ -1,47 +1,57 @@
 from easygui import *
 import toml
 import psycopg2
+import psycopg2.sql as sql
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
 import csv
 
 CONFIG = toml.load("./config.toml") #load variables from toml file
-COLUMNS: list[str] = ['id', 'name', 'clinic', 'issue', 'entrydate', 'open', 'remarks']
-data: list = []
-SELECTED_ROW: int = 0
-OPEN = 'ALL'
+
+class OpenState:
+    OPEN: any = 'ALL'
+    SELECTED_ROW: int = 0
+    data: list = []
+    view_cols: list[str] = ['id', 'name', 'clinic', 'issue', 'entrydate', 'open', 'remarks'] #default view
+    root: any = None #main window object
+    table: any #main table that shows data
+    scrollbar: any #vertical scrollbar
+    hscrollbar: any #horizontal scrollbar
+    main_frame: any
+    button_frame: any
+    image = Image.open("dhr-logo.jpg")
+    image_label: any
 
 def set_open_true():
-    global OPEN
-    OPEN = True
+    OpenState.OPEN = True
     update_data()
 
 def set_open_false():
-    global OPEN
-    OPEN = False
+    OpenState.OPEN = False
     update_data()
 
 def set_all():
-    global OPEN
-    OPEN = 'ALL'
+    OpenState.OPEN = 'ALL'
     update_data()
 
 def select_row(event):
-    global SELECTED_ROW
-    selected_item = table.selection()[0]  # Get the selected item's ID
-    if selected_item:
-        row_data = table.item(selected_item)['values'] # Get data of the selected row
-        SELECTED_ROW = row_data[0]
+    try:
+        selected_item = OpenState.table.selection()[0]  # Get the selected item's ID
+        if selected_item:
+            row_data = OpenState.table.item(selected_item)['values'] # Get data of the selected row
+            OpenState.SELECTED_ROW = row_data[0]
+    except:
+        OpenState.SELECTED_ROW = 0
 
 def delete_row():
-    if SELECTED_ROW == 0:
+    if OpenState.SELECTED_ROW == 0:
         msgbox(title = 'No Row Selected', msg = 'No row selected!', ok_button = 'OK')
     else:
-        con = psycopg2.connect(f'dbname = {CONFIG['credentials']['dbname']} user = {CONFIG['credentials']['username']} password = {CONFIG['credentials']['password']}')
+        con = psycopg2.connect(f'host = {CONFIG['credentials']['host']} dbname = {CONFIG['credentials']['dbname']} user = {CONFIG['credentials']['username']} password = {CONFIG['credentials']['password']}')
         cur = con.cursor()
         SQL = 'SELECT * FROM responses WHERE id = %s;'
-        DATA = SELECTED_ROW
+        DATA = OpenState.SELECTED_ROW
         cur.execute(SQL, (DATA,))
         result = cur.fetchone()[0]
         delete = ynbox(msg = "Really delete this row?", 
@@ -62,80 +72,91 @@ def delete_row():
             update_data()
 
 def update_data():
-    data = [] #if you don't reinitialize this it just adds duplicate entries every time
-    refresh_data(table, data, OPEN)
+    OpenState.data = [] #if you don't reinitialize this it just adds duplicate entries every time
+    refresh_data(OpenState.table, OpenState.data, OpenState.OPEN)
 
-def refresh_data(table, data, OPEN):
-    global SELECTED_ROW
-    SELECTED_ROW = 0
+def refresh_data(table, data, open):
+    OpenState.SELECTED_ROW = 0
     for item in table.get_children():
-        table.delete(item)
-    con = psycopg2.connect(f'dbname = {CONFIG['credentials']['dbname']} user = {CONFIG['credentials']['username']} password = {CONFIG['credentials']['password']}')
+        OpenState.table.delete(item)
+    con = psycopg2.connect(f'host = {CONFIG['credentials']['host']} dbname = {CONFIG['credentials']['dbname']} user = {CONFIG['credentials']['username']} password = {CONFIG['credentials']['password']}')
     cur = con.cursor()
-    DATA = OPEN
-    if OPEN != 'ALL':
-        SQL = 'SELECT * FROM responses WHERE open is %s ORDER BY id ASC;'        
-        cur.execute(SQL, (DATA,))
-    if OPEN == 'ALL':
-        SQL = 'SELECT * FROM responses ORDER by id ASC;'
-        cur.execute(SQL)    
+    DATA = (open,)
+    if open != 'ALL':
+        query = sql.SQL("SELECT {} FROM responses WHERE open IS %s ORDER BY 1 ASC;").format(
+            sql.SQL(",").join(sql.Identifier(column) for column in OpenState.view_cols)
+        )
+        cur.execute(query.as_string(con), DATA)
+    else:
+        query = sql.SQL("SELECT {} FROM responses ORDER BY id ASC;").format(
+            sql.SQL(",").join(sql.Identifier(column) for column in OpenState.view_cols)
+        )
+        cur.execute(query.as_string(con))
     rows = cur.fetchall()
     cur.close()
     con.close()
 
     for row in rows:
         subresult = []
-        for i in range(len(COLUMNS)):
+        for i in range(len(OpenState.view_cols)):
             subresult.append(row[i])
         data.append(subresult)
 
     for row in data:
-        table.insert("", tk.END, values=row)
+        OpenState.table.insert("", tk.END, values=row)
+
+def sort_treeview(tree, col, descending):
+    data = [(OpenState.table.set(item, col), item) for item in OpenState.table.get_children('')]
+    try:
+        data.sort(key = lambda t: int(t[0]), reverse=descending) #special case for ints (like ID)
+    except ValueError:
+        data.sort(reverse=descending) #all other columns
+    
+    for index, (val, item) in enumerate(data):
+        tree.move(item, '', index)
+    OpenState.table.heading(col, command = lambda: sort_treeview(tree, col, not descending))
 
 def search():
-    global table
-    global data
-    global SELECTED_ROW
-
-    SELECTED_ROW = 0
-    data = []
-
+    OpenState.SELECTED_ROW = 0
+    OpenState.data = []
     search_text = enterbox(title = 'Ticket Search', msg = 'Enter search criteria (all parts of all tickets will be searched):', default = '')
-    search_text = '%' + search_text + '%'
-    for item in table.get_children():
-        table.delete(item)
-    con = psycopg2.connect(f'dbname = {CONFIG['credentials']['dbname']} user = {CONFIG['credentials']['username']} password = {CONFIG['credentials']['password']}')
-    cur = con.cursor()
-    DATA = (search_text, search_text, search_text, search_text) #clumsy but works
-    cur.execute("""SELECT * FROM responses WHERE 
-                (issue LIKE %s OR
-                name LIKE %s OR
-                clinic LIKE %s OR
-                remarks LIKE %s)
-                ORDER BY id ASC;""", DATA)
-    rows = cur.fetchall()
- 
-    cur.close()
-    con.close()
+    if search_text != None:
+        search_text = '%' + search_text + '%'
+        for item in OpenState.table.get_children():
+            OpenState.table.delete(item)
+        con = psycopg2.connect(f'host = {CONFIG['credentials']['host']} dbname = {CONFIG['credentials']['dbname']} user = {CONFIG['credentials']['username']} password = {CONFIG['credentials']['password']}')
+        cur = con.cursor()
+        DATA = (search_text, search_text, search_text, search_text) #clumsy but works
+        cur.execute("""SELECT * FROM responses WHERE 
+                    (issue LIKE %s OR
+                    name LIKE %s OR
+                    clinic LIKE %s OR
+                    remarks LIKE %s)
+                    ORDER BY id ASC;""", DATA)
+        rows = cur.fetchall()
 
-    for row in rows:
-        subresult = []
-        for i in range(len(COLUMNS)):
-            subresult.append(row[i])
-        data.append(subresult)
+        cur.close()
+        con.close()
 
-    for row in data:
-        table.insert("", tk.END, values=row)
+        for row in rows:
+            subresult = []
+            for i in range(len(OpenState.view_cols)):
+                subresult.append(row[i])
+            OpenState.data.append(subresult)
+
+        for row in OpenState.data:
+            OpenState.table.insert("", tk.END, values=row)
+        else:
+            pass
 
 def close_ticket():
-    global SELECTED_ROW
-    if SELECTED_ROW == 0:
+    if OpenState.SELECTED_ROW == 0:
         msgbox(title = 'No Ticket Selected', 
                msg = 'You need to select a ticket before you can close it!')    
     else:
-        con = psycopg2.connect(f'dbname = {CONFIG['credentials']['dbname']} user = {CONFIG['credentials']['username']} password = {CONFIG['credentials']['password']}')
+        con = psycopg2.connect(f'host = {CONFIG['credentials']['host']} dbname = {CONFIG['credentials']['dbname']} user = {CONFIG['credentials']['username']} password = {CONFIG['credentials']['password']}')
         cur = con.cursor()
-        cur.execute('SELECT open FROM responses WHERE id = %s', (SELECTED_ROW,))
+        cur.execute('SELECT open FROM responses WHERE id = %s', (OpenState.SELECTED_ROW,))
         result = cur.fetchone()
         if result[0] == False:
             msgbox(title = 'Ticket Already Closed', 
@@ -150,25 +171,51 @@ def close_ticket():
                 pass
             if close_question == True:
                 closing_remarks: str = textbox(title = "Ticket's Closing Remarks", 
-                                            msg = "Please enter your closing remarks for this ticket.",
-                                            text = "None (no remarks given)")
+                                            msg = "Please enter your closing remarks for this ticket.")
 
                 SQL = 'UPDATE responses SET open = False, remarks = %s WHERE id = %s;'
-                DATA = (closing_remarks, SELECTED_ROW)
+                DATA = (closing_remarks, OpenState.SELECTED_ROW)
                 cur.execute(SQL, DATA)        
                 cur.close()
                 con.commit()
-                msgbox(title = 'Ticket Closed', msg = 'The selected ticket has been successfully closed! :)')
+                msgbox(title = 'Ticket Closed', msg = f'The selected ticket (ID: {OpenState.SELECTED_ROW}) has been successfully closed! :)')
                 update_data()
 
+def select_view():
+    OpenState.view_cols = multchoicebox(title = 'View Selector', 
+                                        msg = 'Select the columns you wish to view',
+                                        choices = ['id', 'name', 'clinic', 'issue', 'entrydate', 'open', 'remarks', 'ip_addr', 'hostname'],
+                                        preselect = [0, 1, 2, 3, 4, 5, 6]
+                                        )
+    if OpenState.view_cols == None:
+        msgbox(title = 'Error!',
+               msg = 'At least one selection must be made!')
+        select_view()
+    else:
+        OpenState.button_frame.destroy()
+        OpenState.image_label.destroy()
+        OpenState.table.destroy()
+        OpenState.scrollbar.destroy()
+        OpenState.main_frame.destroy()
+        create_window()
+        update_data()
+
 def generate_report():
-    REPORT_COLUMNS: list[str] = ['id', 'name', 'clinic', 'issue', 'entrydate', 'open', 'remarks', 'ip', 'hostname']
     RESULTS: list[any] = []
 
-    con = psycopg2.connect(f'dbname = {CONFIG['credentials']['dbname']} user = {CONFIG['credentials']['username']} password = {CONFIG['credentials']['password']}')
+    con = psycopg2.connect(f'host = {CONFIG['credentials']['host']} dbname = {CONFIG['credentials']['dbname']} user = {CONFIG['credentials']['username']} password = {CONFIG['credentials']['password']}')
     cur = con.cursor()
 
-    cur.execute("SELECT * FROM responses")
+    if OpenState.OPEN != 'ALL':
+        query = sql.SQL("SELECT {} FROM responses WHERE open IS %s ORDER BY 1 ASC;").format(
+            sql.SQL(",").join(sql.Identifier(column) for column in OpenState.view_cols)
+        )
+        cur.execute(query.as_string(con), (OpenState.OPEN,))
+    else:
+        query = sql.SQL("SELECT {} FROM responses ORDER BY id ASC;").format(
+            sql.SQL(",").join(sql.Identifier(column) for column in OpenState.view_cols)
+        )
+        cur.execute(query.as_string(con))
     rows = cur.fetchall()
 
     cur.close()
@@ -181,75 +228,91 @@ def generate_report():
     if output:
         for row in rows:
             subresult = []
-            for i in range(len(REPORT_COLUMNS)):
+            for i in range(len(OpenState.view_cols)):
                 subresult.append(row[i])
             RESULTS.append(subresult)
 
         with open(output, 'w', newline = '') as report:
             writer = csv.writer(report)
-            writer.writerow(REPORT_COLUMNS)
+            writer.writerow(OpenState.view_cols)
             for row in RESULTS:
                 writer.writerow(row)
 
-root = tk.Tk()
-root.title("Call Center Tickets")
+def create_window():
+    if not OpenState.root:
+        OpenState.root = tk.Tk()
+        OpenState.root.title("Call Center Tickets")
 
-main_frame = ttk.Frame(root)
-main_frame.pack(pady=10)
+    OpenState.main_frame = ttk.Frame(OpenState.root)
+    OpenState.main_frame.pack(pady=10)
 
-table = ttk.Treeview(root, columns=COLUMNS, show="headings", height=20)
-table.pack(side=tk.LEFT, fill=tk.BOTH)
+    OpenState.table = ttk.Treeview(OpenState.root, columns=OpenState.view_cols, selectmode='browse', show="headings", height=20)
+    OpenState.table.pack(side=tk.LEFT, fill=tk.BOTH, expand = True)
 
-# Define column headings
-for column in COLUMNS:
-    table.heading(f'{column}', text=f'{column}')
+    # Define column headings
+    for column in OpenState.view_cols:
+        OpenState.table.heading(f'{column}', text=f'{column}', anchor = tk.CENTER, command = lambda c = f'{column}': sort_treeview(OpenState.table, c, False))
+        OpenState.table.column(column, anchor=tk.CENTER, width = 80)
+    OpenState.table.update()
+    for column in OpenState.table['columns']:
+        OpenState.table.column(column, width = 100, stretch = 0)
 
-scrollbar = ttk.Scrollbar(root, orient=tk.VERTICAL, command=table.yview)
-scrollbar.pack(side=tk.LEFT, fill=tk.Y)
+    OpenState.scrollbar = ttk.Scrollbar(OpenState.root, orient=tk.VERTICAL, command=OpenState.table.yview)
+    OpenState.scrollbar.pack(side=tk.LEFT, fill=tk.Y)
 
-table.configure(yscrollcommand=scrollbar.set)
+    OpenState.hscrollbar = ttk.Scrollbar(OpenState.table, orient=tk.HORIZONTAL, command=OpenState.table.xview)
+    OpenState.hscrollbar.pack(side=tk.BOTTOM, fill=tk.X)
 
-# Button Frame
-button_frame = ttk.Frame(root)
-button_frame.pack(side=tk.TOP, pady = 10)
+    OpenState.table.configure(yscrollcommand=OpenState.scrollbar.set)
+    OpenState.table.configure(xscrollcommand=OpenState.hscrollbar.set)
 
-# Actual Buttons
-show_row = ttk.Button(button_frame, text="Search Tickets", command=search)
-show_row.pack(padx = 5, pady = 5)
+    # Button Frame
+    OpenState.button_frame = ttk.Frame(OpenState.root)
+    OpenState.button_frame.pack(side=tk.TOP, pady = 10)
 
-refresh_button = ttk.Button(button_frame, text="Refresh Database", command=update_data)
-refresh_button.pack(padx = 5, pady = 5)
+    # Actual Buttons
 
-delete_button = ttk.Button(button_frame, text="Delete Row", command=delete_row)
-delete_button.pack(padx = 5, pady = 5)
+    view_change = ttk.Button(OpenState.button_frame, text = 'Change View', command = select_view)
+    view_change.pack(padx = 5, pady = 5)
 
-view_open_tickets = ttk.Button(button_frame, text = 'Show Open Tickets', command = set_open_true)
-view_open_tickets.pack(padx = 5, pady = 5)
+    show_row = ttk.Button(OpenState.button_frame, text="Search Tickets", command=search)
+    show_row.pack(padx = 5, pady = 5)
 
-view_closed_tickets = ttk.Button(button_frame, text = 'Show Closed Tickets', command = set_open_false)
-view_closed_tickets.pack(padx = 5, pady = 5)
+    refresh_button = ttk.Button(OpenState.button_frame, text="Refresh Database", command=update_data)
+    refresh_button.pack(padx = 5, pady = 5)
 
-view_all_tickets = ttk.Button(button_frame, text = 'Show All Tickets', command = set_all)
-view_all_tickets.pack(padx = 5, pady = 5)
+    delete_button = ttk.Button(OpenState.button_frame, text="Delete Row", command=delete_row)
+    delete_button.pack(padx = 5, pady = 5)
 
-close_selected_ticket = ttk.Button(button_frame, text = 'Close Ticket', command = close_ticket)
-close_selected_ticket.pack(padx = 5, pady = 5)
+    view_open_tickets = ttk.Button(OpenState.button_frame, text = 'Show Open Tickets', command = set_open_true)
+    view_open_tickets.pack(padx = 5, pady = 5)
 
-report = ttk.Button(button_frame, text = 'Generate Report of Current View', command = generate_report)
-report.pack(padx = 5, pady = 5)
+    view_closed_tickets = ttk.Button(OpenState.button_frame, text = 'Show Closed Tickets', command = set_open_false)
+    view_closed_tickets.pack(padx = 5, pady = 5)
 
-#Load DHR logo
-image = Image.open("dhr-logo.jpg") # DHR logo
-resized_image = image.resize((400, 110))
-photo = ImageTk.PhotoImage(resized_image)
-image_label = tk.Label(main_frame, image=photo)
-image_label.image = photo # Keep a reference to prevent garbage collection
-image_label.pack(side = tk.BOTTOM)
+    view_all_tickets = ttk.Button(OpenState.button_frame, text = 'Show All Tickets', command = set_all)
+    view_all_tickets.pack(padx = 5, pady = 5)
 
-# Initial table population
-refresh_data(table, data, OPEN)
+    close_selected_ticket = ttk.Button(OpenState.button_frame, text = 'Close Ticket', command = close_ticket)
+    close_selected_ticket.pack(padx = 5, pady = 5)
 
-table.bind("<ButtonRelease-1>", select_row)  # Call select_row on mouse click
+    report = ttk.Button(OpenState.button_frame, text = 'Generate Report of Current View', command = generate_report)
+    report.pack(padx = 5, pady = 5)
 
-table.pack()
-root.mainloop()
+    #Load DHR logo
+    OpenState.image = Image.open("dhr-logo.jpg") # DHR logo
+    resized_image = OpenState.image.resize((400, 110))
+    photo = ImageTk.PhotoImage(resized_image)
+    OpenState.image_label = tk.Label(OpenState.main_frame, image=photo)
+    OpenState.image_label.image = photo # Keep a reference to prevent garbage collection
+    OpenState.image_label.pack(side = tk.BOTTOM)
+
+    # Initial table population
+    refresh_data(OpenState.table, OpenState.data, OpenState.OPEN)
+
+    OpenState.table.bind("<ButtonRelease-1>", select_row)  # Call select_row on mouse click
+
+    OpenState.table.pack()
+
+create_window()
+OpenState.root.mainloop()
